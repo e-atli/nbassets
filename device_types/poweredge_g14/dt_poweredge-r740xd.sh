011f616e77fc5f3fcfb8459aefe512d348c4f340
boris-maier <51813774+boris-maier@users.noreply.github.com>
2019-06-14 10:37:02 +0200

    Update dt_poweredge-r740xd.sh

--- device_types/poweredge_g14/dt_poweredge-r740xd.sh
+++ device_types/poweredge_g14/dt_poweredge-r740xd.sh
@@ -4,8 +4,8 @@ python3 $NETBOX_PATH/manage.py nbshell <<EOF
 
 mnid = Manufacturer.objects.get(slug="dell-emc").id
 
-DeviceType(manufacturer_id=mnid, model="PowerEdge R740xd2", slug="poweredge-r740xd2").save()
-dt = DeviceType.objects.get(slug="poweredge-r740xd2")
+DeviceType(manufacturer_id=mnid, model="PowerEdge R740xd", slug="poweredge-r740xd").save()
+dt = DeviceType.objects.get(slug="poweredge-r740xd")
 dt.u_height="2"
 dt.save()
 
