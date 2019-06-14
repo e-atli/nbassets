fb6e03a7c10df89d7df914319f25d85aaff8eef9
e-atli <e.atli@bsh-it.de>
2019-06-14 10:15:38 +0200

    Commit

--- nbassets.sh
+++ nbassets.sh
@@ -6,11 +6,27 @@ echo
 
 export NETBOX_PATH="/opt/netbox/netbox"
 echo "Netbox Path: $NETBOX_PATH"
+echo
 
 echo "Searching Manufacturers"
+echo "======================="
 find . -name mf_*.sh
+echo
 
 for i in $(find . -name mf_*.sh);
 do
-  ./$i
+  echo "Processing $i..."
+  ./$i > /dev/null 2>&1
+done
+
+echo
+echo "Searching Device Types"
+echo "======================"
+find . -name dt_*.sh
+echo
+
+for i in $(find . -name dt_*.sh);
+do
+  echo "Processing $i..."
+  ./$i > /dev/null 2>&1
 done
