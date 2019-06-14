b6a4278d67973aa98c402020416d132c2f47c078
boris-maier <51813774+boris-maier@users.noreply.github.com>
2019-06-14 11:35:49 +0200

    Add DELL EMC PowerVault MD Device Types

--- device_types/powervault_md/dt_powervault-md3220.sh
+++ device_types/powervault_md/dt_powervault-md3220.sh
@@ -4,8 +4,8 @@ python3 $NETBOX_PATH/manage.py nbshell <<EOF
 
 mnid = Manufacturer.objects.get(slug="dell-emc").id
 
-DeviceType(manufacturer_id=mnid, model="PowerVault MD3200", slug="powervault-md3200").save()
-dt = DeviceType.objects.get(slug="powervault-md3200")
+DeviceType(manufacturer_id=mnid, model="PowerVault MD3220", slug="powervault-md3220").save()
+dt = DeviceType.objects.get(slug="powervault-md3220")
 dt.u_height="2"
 dt.save()
 
