6b6b4f65b069d9f0372fdb32b17607a1e034d2b5
boris-maier <51813774+boris-maier@users.noreply.github.com>
2019-06-14 11:39:54 +0200

    update

--- device_types/powervault_md/dt_powervault-md3400.sh
+++ device_types/powervault_md/dt_powervault-md3400.sh
@@ -5,7 +5,7 @@ python3 $NETBOX_PATH/manage.py nbshell <<EOF
 mnid = Manufacturer.objects.get(slug="dell-emc").id
 
 DeviceType(manufacturer_id=mnid, model="PowerVault MD3400", slug="powervault-md3400").save()
-dt = DeviceType.objects.get(slug="powerVault-md3400")
+dt = DeviceType.objects.get(slug="powervault-md3400")
 dt.u_height="2"
 dt.save()
 
--- device_types/powervault_md/dt_powervault-md3420.sh
+++ device_types/powervault_md/dt_powervault-md3420.sh
@@ -5,7 +5,7 @@ python3 $NETBOX_PATH/manage.py nbshell <<EOF
 mnid = Manufacturer.objects.get(slug="dell-emc").id
 
 DeviceType(manufacturer_id=mnid, model="PowerVault MD3420", slug="powervault-md3420").save()
-dt = DeviceType.objects.get(slug="powerVault-md3420")
+dt = DeviceType.objects.get(slug="powervault-md3420")
 dt.u_height="2"
 dt.save()
 
