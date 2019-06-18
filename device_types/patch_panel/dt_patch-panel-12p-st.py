5f5c19179a0da01318d46318dcf3691f5edbfe30
Eren Atli <E.Atli@bsh-it.de>
2019-06-18 10:39:14 +0200

    Create dt_patch-panel-12p-st.py

---
+++ device_types/patch_panel/dt_patch-panel-12p-st.py
@@ -0,0 +1,33 @@
+MFGSLUG = "generic"
+SLUG = "patch-panel-12p-st"
+MODEL = "Patch panel 12x ST Port"
+U_HEIGHT = "1"
+IS_FULL_DEPTH = False
+
+RearPortTemplate(device_type_id=NEW_DEVICE_TYPE.id, name="Port 1").save()
+
+NEW_REAR_PORT_TEMPLATE = RearPortTemplate(device_type_id=NEW_DEVICE_TYPE.id, name="Port 1")
+NEW_INTERFACE_TEMPLATE.type = "2000"
+NEW_INTERFACE_TEMPLATE.save()
+
+#PORT_TYPE_8P8C = 1000
+#PORT_TYPE_110_PUNCH = 1100
+#PORT_TYPE_ST = 2000
+#PORT_TYPE_SC = 2100
+#PORT_TYPE_SC_APC = 2110
+#PORT_TYPE_FC = 2200
+#PORT_TYPE_LC = 2300
+#PORT_TYPE_LC_APC = 2310
+#PORT_TYPE_MTRJ = 2400
+#PORT_TYPE_MPO = 2500
+#PORT_TYPE_LSH = 2600
+#PORT_TYPE_LSH_APC = 2610
+#PORT_TYPE_CHOICES = [
+ 
+#  |  device_type_id = <django.db.models.query_utils.DeferredAttribute object>
+# |  get_type_display = _method(self, *, field=<django.db.models.fields.PositiveSmallIntegerField: type>)
+# |
+# |  id = <django.db.models.query_utils.DeferredAttribute object>
+# |  name = <django.db.models.query_utils.DeferredAttribute object>
+# |  positions = <django.db.models.query_utils.DeferredAttribute object>
+# |  type = <django.db.models.query_utils.DeferredAttribute object>
