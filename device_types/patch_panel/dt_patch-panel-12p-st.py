7cd1bfc0dd8dca88115629aaedc87bae4c979bb2
Eren Atli <E.Atli@bsh-it.de>
2019-06-18 12:03:00 +0200

    Update dt_patch-panel-12p-st.py

--- device_types/patch_panel/dt_patch-panel-12p-st.py
+++ device_types/patch_panel/dt_patch-panel-12p-st.py
@@ -4,6 +4,7 @@ MODEL = "Patch Panel 12 Ports ST"
 U_HEIGHT = "1"
 IS_FULL_DEPTH = False
 
+# Rear Port 1/2
 NEW_REAR_PORT_TEMPLATE = RearPortTemplate()
 NEW_REAR_PORT_TEMPLATE.device_type_id = NEW_DEVICE_TYPE.id
 NEW_REAR_PORT_TEMPLATE.name = "Port 1/2"
@@ -11,6 +12,7 @@ NEW_REAR_PORT_TEMPLATE.type = "2000"
 NEW_REAR_PORT_TEMPLATE.positions = "1"
 NEW_REAR_PORT_TEMPLATE.save()
 
+# Front Port 1/2
 NEW_FRONT_PORT_TEMPLATE = FrontPortTemplate()
 NEW_FRONT_PORT_TEMPLATE.device_type_id = NEW_DEVICE_TYPE.id
 NEW_FRONT_PORT_TEMPLATE.name = "Port 1/2"
@@ -19,71 +21,87 @@ NEW_FRONT_PORT_TEMPLATE.type = "2000"
 NEW_FRONT_PORT_TEMPLATE.rear_port_position = "1"
 NEW_FRONT_PORT_TEMPLATE.save()
 
+# Rear Port 3/4
+NEW_REAR_PORT_TEMPLATE = RearPortTemplate()
+NEW_REAR_PORT_TEMPLATE.device_type_id = NEW_DEVICE_TYPE.id
+NEW_REAR_PORT_TEMPLATE.name = "Port 3/4"
+NEW_REAR_PORT_TEMPLATE.type = "2000"
+NEW_REAR_PORT_TEMPLATE.positions = "1"
+NEW_REAR_PORT_TEMPLATE.save()
 
+# Front Port 3/4
+NEW_FRONT_PORT_TEMPLATE = FrontPortTemplate()
+NEW_FRONT_PORT_TEMPLATE.device_type_id = NEW_DEVICE_TYPE.id
+NEW_FRONT_PORT_TEMPLATE.name = "Port 3/4"
+NEW_FRONT_PORT_TEMPLATE.rear_port_id = NEW_REAR_PORT_TEMPLATE.id
+NEW_FRONT_PORT_TEMPLATE.type = "2000"
+NEW_FRONT_PORT_TEMPLATE.rear_port_position = "1"
+NEW_FRONT_PORT_TEMPLATE.save()
 
+# Rear Port 5/6
+NEW_REAR_PORT_TEMPLATE = RearPortTemplate()
+NEW_REAR_PORT_TEMPLATE.device_type_id = NEW_DEVICE_TYPE.id
+NEW_REAR_PORT_TEMPLATE.name = "Port 5/6"
+NEW_REAR_PORT_TEMPLATE.type = "2000"
+NEW_REAR_PORT_TEMPLATE.positions = "1"
+NEW_REAR_PORT_TEMPLATE.save()
 
-#RearPortTemplate(device_type_id=NEW_DEVICE_TYPE.id, name="Port 3/4").save()
-#NEW_REAR_PORT_TEMPLATE = RearPortTemplate.objects.get(device_type_id=NEW_DEVICE_TYPE.id, name="Port 3/4")
-#NEW_REAR_PORT_TEMPLATE.type = "2000"
-#NEW_REAR_PORT_TEMPLATE.positions = "1"
-#NEW_REAR_PORT_TEMPLATE.save()###
-#
-#FrontPortTemplate(device_type_id=NEW_DEVICE_TYPE.id, rear_port_id=NEW_REAR_PORT_TEMPLATE.id, name="Port 3/4", rear_port_position="1").save()
+# Front Port 5/6
+NEW_FRONT_PORT_TEMPLATE = FrontPortTemplate()
+NEW_FRONT_PORT_TEMPLATE.device_type_id = NEW_DEVICE_TYPE.id
+NEW_FRONT_PORT_TEMPLATE.name = "Port 5/6"
+NEW_FRONT_PORT_TEMPLATE.rear_port_id = NEW_REAR_PORT_TEMPLATE.id
+NEW_FRONT_PORT_TEMPLATE.type = "2000"
+NEW_FRONT_PORT_TEMPLATE.rear_port_position = "1"
+NEW_FRONT_PORT_TEMPLATE.save()
 
-#RearPortTemplate(device_type_id=NEW_DEVICE_TYPE.id, name="Port 5/6").save()#
-#NEW_REAR_PORT_TEMPLATE = RearPortTemplate.objects.get(device_type_id=NEW_DEVICE_TYPE.id, name="Port 5/6")
-#NEW_REAR_PORT_TEMPLATE.type = "2000"
-#NEW_REAR_PORT_TEMPLATE.positions = "1"
-#NEW_REAR_PORT_TEMPLATE.save()
+# Rear Port 7/8
+NEW_REAR_PORT_TEMPLATE = RearPortTemplate()
+NEW_REAR_PORT_TEMPLATE.device_type_id = NEW_DEVICE_TYPE.id
+NEW_REAR_PORT_TEMPLATE.name = "Port 7/8"
+NEW_REAR_PORT_TEMPLATE.type = "2000"
+NEW_REAR_PORT_TEMPLATE.positions = "1"
+NEW_REAR_PORT_TEMPLATE.save()
 
-#RearPortTemplate(device_type_id=NEW_DEVICE_TYPE.id, name="Port 7/8").save()
-#NEW_REAR_PORT_TEMPLATE = RearPortTemplate.objects.get(device_type_id=NEW_DEVICE_TYPE.id, name="Port 7/8")
-#NEW_REAR_PORT_TEMPLATE.type = "2000"
-#NEW_REAR_PORT_TEMPLATE.positions = "1"
-#NEW_REAR_PORT_TEMPLATE.save()
+# Front Port 7/8
+NEW_FRONT_PORT_TEMPLATE = FrontPortTemplate()
+NEW_FRONT_PORT_TEMPLATE.device_type_id = NEW_DEVICE_TYPE.id
+NEW_FRONT_PORT_TEMPLATE.name = "Port 7/8"
+NEW_FRONT_PORT_TEMPLATE.rear_port_id = NEW_REAR_PORT_TEMPLATE.id
+NEW_FRONT_PORT_TEMPLATE.type = "2000"
+NEW_FRONT_PORT_TEMPLATE.rear_port_position = "1"
+NEW_FRONT_PORT_TEMPLATE.save()
 
-#RearPortTemplate(device_type_id=NEW_DEVICE_TYPE.id, name="Port 9/10").save()
-#NEW_REAR_PORT_TEMPLATE = RearPortTemplate.objects.get(device_type_id=NEW_DEVICE_TYPE.id, name="Port 9/10")
-#NEW_REAR_PORT_TEMPLATE.type = "2000"
-#NEW_REAR_PORT_TEMPLATE.positions = "1"
-#NEW_REAR_PORT_TEMPLATE.save()
+# Rear Port 9/10
+NEW_REAR_PORT_TEMPLATE = RearPortTemplate()
+NEW_REAR_PORT_TEMPLATE.device_type_id = NEW_DEVICE_TYPE.id
+NEW_REAR_PORT_TEMPLATE.name = "Port 9/10"
+NEW_REAR_PORT_TEMPLATE.type = "2000"
+NEW_REAR_PORT_TEMPLATE.positions = "1"
+NEW_REAR_PORT_TEMPLATE.save()
 
-#RearPortTemplate(device_type_id=NEW_DEVICE_TYPE.id, name="Port 11/12").save()
-#NEW_REAR_PORT_TEMPLATE = RearPortTemplate.objects.get(device_type_id=NEW_DEVICE_TYPE.id, name="Port 11/12")
-#NEW_REAR_PORT_TEMPLATE.type = "2000"
-#NEW_REAR_PORT_TEMPLATE.positions = "1"
-#NEW_REAR_PORT_TEMPLATE.save()
+# Front Port 9/10
+NEW_FRONT_PORT_TEMPLATE = FrontPortTemplate()
+NEW_FRONT_PORT_TEMPLATE.device_type_id = NEW_DEVICE_TYPE.id
+NEW_FRONT_PORT_TEMPLATE.name = "Port 9/10"
+NEW_FRONT_PORT_TEMPLATE.rear_port_id = NEW_REAR_PORT_TEMPLATE.id
+NEW_FRONT_PORT_TEMPLATE.type = "2000"
+NEW_FRONT_PORT_TEMPLATE.rear_port_position = "1"
+NEW_FRONT_PORT_TEMPLATE.save()
 
-#PORT_TYPE_8P8C = 1000
-#PORT_TYPE_110_PUNCH = 1100
-#PORT_TYPE_ST = 2000
-#PORT_TYPE_SC = 2100
-#PORT_TYPE_SC_APC = 2110
-#PORT_TYPE_FC = 2200
-#PORT_TYPE_LC = 2300
-#PORT_TYPE_LC_APC = 2310
-#PORT_TYPE_MTRJ = 2400
-#PORT_TYPE_MPO = 2500
-#PORT_TYPE_LSH = 2600
-#PORT_TYPE_LSH_APC = 2610
-#PORT_TYPE_CHOICES = [
- 
-#  |  device_type_id = <django.db.models.query_utils.DeferredAttribute object>
-# |  get_type_display = _method(self, *, field=<django.db.models.fields.PositiveSmallIntegerField: type>)
-# |
-# |  id = <django.db.models.query_utils.DeferredAttribute object>
-# |  name = <django.db.models.query_utils.DeferredAttribute object>
-# |  positions = <django.db.models.query_utils.DeferredAttribute object>
-# |  type = <django.db.models.query_utils.DeferredAttribute object>
+# Rear Port 11/12
+NEW_REAR_PORT_TEMPLATE = RearPortTemplate()
+NEW_REAR_PORT_TEMPLATE.device_type_id = NEW_DEVICE_TYPE.id
+NEW_REAR_PORT_TEMPLATE.name = "Port 11/12"
+NEW_REAR_PORT_TEMPLATE.type = "2000"
+NEW_REAR_PORT_TEMPLATE.positions = "1"
+NEW_REAR_PORT_TEMPLATE.save()
 
-
-
- #|  device_type_id = <django.db.models.query_utils.DeferredAttribute object>
- #|  get_type_display = _method(self, *, field=<django.db.models.fields.PositiveSmallIntegerField: type>)
- #|
- #|  id = <django.db.models.query_utils.DeferredAttribute object>
- #|  name = <django.db.models.query_utils.DeferredAttribute object>
- #|  rear_port_id = <django.db.models.query_utils.DeferredAttribute object>
- #|  rear_port_position = <django.db.models.query_utils.DeferredAttribute object>
- #|  type = <django.db.models.query_utils.DeferredAttribute object>
- 
+# Front Port 11/12
+NEW_FRONT_PORT_TEMPLATE = FrontPortTemplate()
+NEW_FRONT_PORT_TEMPLATE.device_type_id = NEW_DEVICE_TYPE.id
+NEW_FRONT_PORT_TEMPLATE.name = "Port 11/12"
+NEW_FRONT_PORT_TEMPLATE.rear_port_id = NEW_REAR_PORT_TEMPLATE.id
+NEW_FRONT_PORT_TEMPLATE.type = "2000"
+NEW_FRONT_PORT_TEMPLATE.rear_port_position = "1"
+NEW_FRONT_PORT_TEMPLATE.save()
