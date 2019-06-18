d6f211ef62d5cb7d3deb7ff541c8f67fe22d05e9
Eren Atli <E.Atli@bsh-it.de>
2019-06-18 11:49:23 +0200

    Update dt_patch-panel-12p-st.py

--- device_types/patch_panel/dt_patch-panel-12p-st.py
+++ device_types/patch_panel/dt_patch-panel-12p-st.py
@@ -4,45 +4,51 @@ MODEL = "Patch Panel 12 Ports ST"
 U_HEIGHT = "1"
 IS_FULL_DEPTH = False
 
-RearPortTemplate(device_type_id=NEW_DEVICE_TYPE.id, name="Port 1/2").save()
-NEW_REAR_PORT_TEMPLATE = RearPortTemplate.objects.get(device_type_id=NEW_DEVICE_TYPE.id, name="Port 1/2")
+NEW_REAR_PORT_TENPLATE = RearPortTemplate(device_type_id=NEW_DEVICE_TYPE.id, name="Port 1/2")
+#NEW_REAR_PORT_TEMPLATE = RearPortTemplate.objects.get(device_type_id=NEW_DEVICE_TYPE.id, name="Port 1/2")
 NEW_REAR_PORT_TEMPLATE.type = "2000"
 NEW_REAR_PORT_TEMPLATE.positions = "1"
 NEW_REAR_PORT_TEMPLATE.save()
 
-FrontPortTemplate(device_type_id=NEW_DEVICE_TYPE.id, rear_port_id=NEW_REAR_PORT_TEMPLATE.id, name="Port 1/2", rear_port_position="1").save()
+NEW_FRONT_PORT_TEMPLATE = FrontPortTemplate(device_type_id=NEW_DEVICE_TYPE.id, rear_port_id=NEW_REAR_PORT_TEMPLATE.id, name="Port 1/2")
+NEW_FRONT_PORT_TEMPLATE.type = "2000"
+NEW_FRONT_PORT_TEMPLATE.rear_port_position = "1"
+NEW_FRONT_PORT_TEMPLATE.save()
 
-RearPortTemplate(device_type_id=NEW_DEVICE_TYPE.id, name="Port 3/4").save()
-NEW_REAR_PORT_TEMPLATE = RearPortTemplate.objects.get(device_type_id=NEW_DEVICE_TYPE.id, name="Port 3/4")
-NEW_REAR_PORT_TEMPLATE.type = "2000"
-NEW_REAR_PORT_TEMPLATE.positions = "1"
-NEW_REAR_PORT_TEMPLATE.save()
 
-FrontPortTemplate(device_type_id=NEW_DEVICE_TYPE.id, rear_port_id=NEW_REAR_PORT_TEMPLATE.id, name="Port 3/4", rear_port_position="1").save()
 
-RearPortTemplate(device_type_id=NEW_DEVICE_TYPE.id, name="Port 5/6").save()
-NEW_REAR_PORT_TEMPLATE = RearPortTemplate.objects.get(device_type_id=NEW_DEVICE_TYPE.id, name="Port 5/6")
-NEW_REAR_PORT_TEMPLATE.type = "2000"
-NEW_REAR_PORT_TEMPLATE.positions = "1"
-NEW_REAR_PORT_TEMPLATE.save()
 
-RearPortTemplate(device_type_id=NEW_DEVICE_TYPE.id, name="Port 7/8").save()
-NEW_REAR_PORT_TEMPLATE = RearPortTemplate.objects.get(device_type_id=NEW_DEVICE_TYPE.id, name="Port 7/8")
-NEW_REAR_PORT_TEMPLATE.type = "2000"
-NEW_REAR_PORT_TEMPLATE.positions = "1"
-NEW_REAR_PORT_TEMPLATE.save()
+#RearPortTemplate(device_type_id=NEW_DEVICE_TYPE.id, name="Port 3/4").save()
+#NEW_REAR_PORT_TEMPLATE = RearPortTemplate.objects.get(device_type_id=NEW_DEVICE_TYPE.id, name="Port 3/4")
+#NEW_REAR_PORT_TEMPLATE.type = "2000"
+#NEW_REAR_PORT_TEMPLATE.positions = "1"
+#NEW_REAR_PORT_TEMPLATE.save()###
+#
+#FrontPortTemplate(device_type_id=NEW_DEVICE_TYPE.id, rear_port_id=NEW_REAR_PORT_TEMPLATE.id, name="Port 3/4", rear_port_position="1").save()
 
-RearPortTemplate(device_type_id=NEW_DEVICE_TYPE.id, name="Port 9/10").save()
-NEW_REAR_PORT_TEMPLATE = RearPortTemplate.objects.get(device_type_id=NEW_DEVICE_TYPE.id, name="Port 9/10")
-NEW_REAR_PORT_TEMPLATE.type = "2000"
-NEW_REAR_PORT_TEMPLATE.positions = "1"
-NEW_REAR_PORT_TEMPLATE.save()
+#RearPortTemplate(device_type_id=NEW_DEVICE_TYPE.id, name="Port 5/6").save()#
+#NEW_REAR_PORT_TEMPLATE = RearPortTemplate.objects.get(device_type_id=NEW_DEVICE_TYPE.id, name="Port 5/6")
+#NEW_REAR_PORT_TEMPLATE.type = "2000"
+#NEW_REAR_PORT_TEMPLATE.positions = "1"
+#NEW_REAR_PORT_TEMPLATE.save()
 
-RearPortTemplate(device_type_id=NEW_DEVICE_TYPE.id, name="Port 11/12").save()
-NEW_REAR_PORT_TEMPLATE = RearPortTemplate.objects.get(device_type_id=NEW_DEVICE_TYPE.id, name="Port 11/12")
-NEW_REAR_PORT_TEMPLATE.type = "2000"
-NEW_REAR_PORT_TEMPLATE.positions = "1"
-NEW_REAR_PORT_TEMPLATE.save()
+#RearPortTemplate(device_type_id=NEW_DEVICE_TYPE.id, name="Port 7/8").save()
+#NEW_REAR_PORT_TEMPLATE = RearPortTemplate.objects.get(device_type_id=NEW_DEVICE_TYPE.id, name="Port 7/8")
+#NEW_REAR_PORT_TEMPLATE.type = "2000"
+#NEW_REAR_PORT_TEMPLATE.positions = "1"
+#NEW_REAR_PORT_TEMPLATE.save()
+
+#RearPortTemplate(device_type_id=NEW_DEVICE_TYPE.id, name="Port 9/10").save()
+#NEW_REAR_PORT_TEMPLATE = RearPortTemplate.objects.get(device_type_id=NEW_DEVICE_TYPE.id, name="Port 9/10")
+#NEW_REAR_PORT_TEMPLATE.type = "2000"
+#NEW_REAR_PORT_TEMPLATE.positions = "1"
+#NEW_REAR_PORT_TEMPLATE.save()
+
+#RearPortTemplate(device_type_id=NEW_DEVICE_TYPE.id, name="Port 11/12").save()
+#NEW_REAR_PORT_TEMPLATE = RearPortTemplate.objects.get(device_type_id=NEW_DEVICE_TYPE.id, name="Port 11/12")
+#NEW_REAR_PORT_TEMPLATE.type = "2000"
+#NEW_REAR_PORT_TEMPLATE.positions = "1"
+#NEW_REAR_PORT_TEMPLATE.save()
 
 #PORT_TYPE_8P8C = 1000
 #PORT_TYPE_110_PUNCH = 1100
