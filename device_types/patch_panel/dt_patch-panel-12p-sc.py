d252e92fdc5e767513ede470f53c56d18833a201
Eren Atli <E.Atli@bsh-it.de>
2019-06-18 12:56:09 +0200

    Create dt_patch-panel-12p-sc.py

---
+++ device_types/patch_panel/dt_patch-panel-12p-sc.py
@@ -0,0 +1,20 @@
+MFGSLUG = "generic"
+SLUG = "patch-panel-12p-sc"
+MODEL = "Patch Panel 12 Ports SC"
+U_HEIGHT = "1"
+IS_FULL_DEPTH = False
+
+# 12x SC-Port
+R_F_TYPE = "2100"
+R_F_PORT = "Port 1"
+R_F_PORT = "Port 2"
+R_F_PORT = "Port 3"
+R_F_PORT = "Port 4"
+R_F_PORT = "Port 5"
+R_F_PORT = "Port 6"
+R_F_PORT = "Port 7"
+R_F_PORT = "Port 8"
+R_F_PORT = "Port 9"
+R_F_PORT = "Port 10"
+R_F_PORT = "Port 11"
+R_F_PORT = "Port 12"
