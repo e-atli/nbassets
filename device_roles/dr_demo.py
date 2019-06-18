7b6db2ec7c60c62296a039a1fd5527883ea8cf1b
Eren Atli <E.Atli@bsh-it.de>
2019-06-18 08:49:36 +0200

    Create dr_demo.py

---
+++ device_roles/dr_demo.py
@@ -0,0 +1,14 @@
+SLUG = 'demo'
+NAME = 'Demo Role'
+COLOR = '2196f3'
+
+#    'teal': '009688',
+#    'green': '4caf50',
+#    'blue': '2196f3',
+#    'purple': '9c27b0',
+#    'yellow': 'ffeb3b',
+#    'orange': 'ff9800',
+#    'red': 'f44336',
+#    'light_gray': 'c0c0c0',
+#    'medium_gray': '9e9e9e',
+#    'dark_gray': '607d8b',
