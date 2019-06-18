6f6f7f58042201bb057f212034663cd45a7f09fc
Eren Atli <E.Atli@bsh-it.de>
2019-06-18 13:56:31 +0200

    Update clean.py

--- python/clean.py
+++ python/clean.py
@@ -21,16 +21,30 @@ for filename in Path('device_roles').glob('**/dr_*'):
     except:
         pass
 
-#for filename in Path('device_types').glob('**/dt_*'):
-#    SLUG = ''
-#    with filename.open() as fp:
-#        line = fp.readline()
-#        while line:
-#            if (line.startswith('SLUG')) :
-#                try:
-#                    exec(line)
-#                    DeviceType.objects.get(slug=SLUG).delete()
-#                except:
-#                    pass
+for filename in Path('device_types').glob('**/dt_*'):
+    SLUG = ''
+    with filename.open() as fp:
+        line = fp.readline()
+        while line:
+            if (line.startswith('SLUG')) :
+                try:
+                    exec(line)
+                    DeviceType.objects.get(slug=SLUG).delete()
+                except:
+                    pass
+            line = fp.readline()
+
+for filename in Path('maufacturers').glob('**/mf_*'):
+    SLUG = ''
+    with filename.open() as fp:
+        line = fp.readline()
+        while line:
+            if (line.startswith('SLUG')) :
+                try:
+                    exec(line)
+                    Manufacturers.objects.get(slug=SLUG).delete()
+                except:
+                    pass
+            line = fp.readline()
 
 quit()
