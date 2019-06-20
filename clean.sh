be69ecd6aba0be0b77df63f1b5d4ae3be9406b3c
e-atli <51813781+e-atli@users.noreply.github.com>
2019-06-20 14:14:19 +0200

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -10,7 +10,8 @@ echo "Cleaning all unused Cluster Types..."
 echo "Cleaning all unused Device Roles..."
 echo "Cleaning all unused Device Types..."
 echo "Cleaning all unused Manufacturers..."
-python3 $NETBOX_PATH/manage.py nbshell <python/clean.py > /dev/null 2>&1
+python3 $NETBOX_PATH/manage.py nbshell <python/clean.py
+#python3 $NETBOX_PATH/manage.py nbshell <python/clean.py > /dev/null 2>&1
 
 echo
 echo "Done."
