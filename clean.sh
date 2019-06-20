49d279650ad52ea305489ae1aecc159fdf2846a4
e-atli <51813781+e-atli@users.noreply.github.com>
2019-06-20 14:15:59 +0200

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -6,10 +6,8 @@ export NETBOX_PATH="/opt/netbox/netbox"
 echo "Netbox Path: $NETBOX_PATH"
 echo
 
-echo "Cleaning all unused Cluster Types..."
-echo "Cleaning all unused Device Roles..."
-echo "Cleaning all unused Device Types..."
-echo "Cleaning all unused Manufacturers..."
+echo "Cleaning all unused Types, Roles, Manufacturers..."
+echo
 python3 $NETBOX_PATH/manage.py nbshell <python/clean.py
 #python3 $NETBOX_PATH/manage.py nbshell <python/clean.py > /dev/null 2>&1
 
