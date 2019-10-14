0777715315b191f3dd5a44ffcd74878d6954ac07
e-atli <51813781+e-atli@users.noreply.github.com>
2019-10-14 13:53:12 +0200

    Create dt_data_domain_dd6300.py

---
+++ device_types/dell-emc/data_domain/dt_data_domain_dd6300.py
@@ -0,0 +1,18 @@
+MFGSLUG = 'dell-emc'
+SLUG = 'data_domain_dd6300'
+MODEL = 'Data Domain DD6300'
+U_HEIGHT = '2'
+IS_FULL_DEPTH = True
+
+IFACE_FF = '1000'
+INTERFACE = 'ethMa'
+INTERFACE = 'eth1a'
+INTERFACE = 'eth1b'
+INTERFACE = 'eth1c'
+INTERFACE = 'eth1d'
+
+IFACE_FF = '1200'
+INTERFACE = 'eth3a'
+INTERFACE = 'eth3b'
+INTERFACE = 'eth3c'
+INTERFACE = 'eth3d'
