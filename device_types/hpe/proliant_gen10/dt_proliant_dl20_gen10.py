f010b2f86c5ff17dbd30b8fa4e7fee17f569938e
e-atli <51813781+e-atli@users.noreply.github.com>
2019-10-14 11:51:23 +0200

    oob

--- device_types/hpe/proliant_gen10/dt_proliant_dl20_gen10.py
+++ device_types/hpe/proliant_gen10/dt_proliant_dl20_gen10.py
@@ -7,14 +7,4 @@ IS_FULL_DEPTH = True
 POWER_PORT = 'PS1'
 
 IFACE_FF = '1000'
-#INTERFACE = 'Port 1'
-#INTERFACE = 'Port 2'
-OOB_MGMT = 'iDRAC'
-
-#
-#InterfaceTemplate(device_type_id=NEW_DEVICE_TYPE.id, name="iLO").save()
-#
-#NEW_INTERFACE_TEMPLATE = InterfaceTemplate.objects.get(device_type_id=NEW_DEVICE_TYPE.id, name="iLO")
-#NEW_INTERFACE_TEMPLATE.form_factor = "1000"
-#NEW_INTERFACE_TEMPLATE.mgmt_only = True
-#NEW_INTERFACE_TEMPLATE.save()
+OOB_MGMT = 'iLO'
