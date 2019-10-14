b180e79790003ed13fa8d6be66a1b876991c0af3
boris-maier <51813774+boris-maier@users.noreply.github.com>
2019-10-14 10:14:48 +0200

    anpassung hpe device Types gen10

---
+++ device_types/hpe/proliant_gen10/dt_proliant_dl160_gen10.py
@@ -0,0 +1,15 @@
+MFGSLUG = "hpe"
+SLUG = "proliant-dl160-gen10"
+MODEL = "ProLiant DL160 Gen10"
+U_HEIGHT = "1"
+IS_FULL_DEPTH = True
+
+POWER_PORT = "PS1"
+POWER_PORT = "PS2"
+
+InterfaceTemplate(device_type_id=NEW_DEVICE_TYPE.id, name="iLO").save()
+
+NEW_INTERFACE_TEMPLATE = InterfaceTemplate.objects.get(device_type_id=NEW_DEVICE_TYPE.id, name="iLO")
+NEW_INTERFACE_TEMPLATE.form_factor = "1000"
+NEW_INTERFACE_TEMPLATE.mgmt_only = True
+NEW_INTERFACE_TEMPLATE.save()
