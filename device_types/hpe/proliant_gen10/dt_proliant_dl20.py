bc399dff665509b3bdd42e0b4723f7f42dd5d67d
e-atli <51813781+e-atli@users.noreply.github.com>
2019-10-14 10:22:59 +0200

    demo

--- device_types/hpe/proliant_gen10/dt_proliant_dl20.py
+++ device_types/hpe/proliant_gen10/dt_proliant_dl20.py
@@ -1,10 +1,10 @@
-MFGSLUG = "hpe"
-SLUG = "proliant-dl20-gen10"
-MODEL = "ProLiant DL20 Gen10"
-U_HEIGHT = "1"
+MFGSLUG = 'hpe'
+SLUG = 'proliant-dl20-gen10'
+MODEL = 'ProLiant DL20 Gen10'
+U_HEIGHT = '1'
 IS_FULL_DEPTH = True
 
-POWER_PORT = "PS1"
+POWER_PORT = 'PS1'
 
 IFACE_FF = '1000'
 OOB_MGMT = 'iLO'
