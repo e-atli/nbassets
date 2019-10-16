02b42b8f97ba4a250091d9b1964e576884005e00
e-atli <51813781+e-atli@users.noreply.github.com>
2019-10-16 09:57:24 +0200

    OOB MGMT Update

--- device_types/dell-emc/poweredge_g12/dt_poweredge-t320.py
+++ device_types/dell-emc/poweredge_g12/dt_poweredge-t320.py
@@ -7,9 +7,5 @@ IS_FULL_DEPTH = True
 POWER_PORT = "PS1"
 POWER_PORT = "PS2"
 
-InterfaceTemplate(device_type_id=NEW_DEVICE_TYPE.id, name="iDRAC").save()
-
-NEW_INTERFACE_TEMPLATE = InterfaceTemplate.objects.get(device_type_id=NEW_DEVICE_TYPE.id, name="iDRAC")
-NEW_INTERFACE_TEMPLATE.form_factor="1000"
-NEW_INTERFACE_TEMPLATE.mgmt_only=True
-NEW_INTERFACE_TEMPLATE.save()
+IFACE_FF = '1000'
+OOB_MGMT = 'iDRAC'
