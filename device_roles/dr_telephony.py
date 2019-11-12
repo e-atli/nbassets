c27d81becb4b26c980b9c94ab0047e1f729e5a6e
Eren Atli <E.Atli@bsh-it.de>
2019-11-12 14:39:25 +0100

    More Roles

---
+++ device_roles/dr_telephony.py
@@ -0,0 +1,5 @@
+SLUG = 'telephony'
+NAME = 'Telephony'
+#    ('e91e63', 'Pink'),
+COLOR = 'e91e63'
+VM_ROLE = True
---
+++ device_roles/dr_web-server.py
@@ -0,0 +1,5 @@
+SLUG = 'web-server'
+NAME = 'Web Server'
+#    ('673ab7', 'Dark purple'),
+COLOR = '673ab7'
+VM_ROLE = True
