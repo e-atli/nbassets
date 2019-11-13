37e1729cf589e9cb5274c2c51a5f12ec61ca6dc5
e-atli <51813781+e-atli@users.noreply.github.com>
2019-11-13 15:22:29 +0100

    Update nbenv.sh

--- nbenv.sh
+++ nbenv.sh
@@ -54,8 +54,8 @@ for (( i=0; i<$NUMVCENTERS; i++ ))
 {
   echo "Connecting to vCenter Server ${VCENTER[$i]}...";
   
-  pwsh powershell/read_clusters.ps1 ${VCENTER[$i]} $USER $PASS > clusters/cl_${VCENTER[$i]}.py
-  source clusters/cl_vmware-vsphere.py
+  pwsh powershell/read_clusters.ps1 ${VCENTER[$i]} $USER $PASS > clusters/cl_${VCENTER[$i]}.py;
+  source clusters/cl_${VCENTER[$i]}.py;
   pwsh powershell/read_vms.ps1 ${VCENTER[$i]} $USER $PASS > virtual_machines/vm_$NAME.py
 }
 
