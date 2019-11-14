2851daeba9908f2989efebb216642649600403ee
e-atli <51813781+e-atli@users.noreply.github.com>
2019-11-14 12:54:39 +0100

    ps1 updates

--- powershell/read_vms.ps1
+++ powershell/read_vms.ps1
@@ -16,32 +16,37 @@ Connect-VIServer -Server $VCENTER -User $USER -Password $PASS | Out-Null
 $CLUSTERS = Get-Cluster
 
 Foreach ($CLUSTER in $CLUSTERS) {
-  "CLUSTER = '$CLUSTER'" | Out-File -FilePath $PATHPREFIX/vm_$CLUSTER.py
+  # Save Cluster Information
+  "TYPE = 'VMware vSphere'" | Out-File -FilePath $PATHPREFIX/clusters/cl_$CLUSTER.py
+  "CLUSTER = '$CLUSTER'" | Out-File -FilePath $PATHPREFIX/clusters/cl_$CLUSTER.py -Append
+  
+  # Save VM Information
+  "CLUSTER = '$CLUSTER'" | Out-File -FilePath $PATHPREFIX/virtual_maschines/vm_$CLUSTER.py
   
   $VMS = Get-VM -Location $CLUSTER
 
   Foreach ($VM in $VMS) {
     $NAME = $VM.Name
-    "NAME = '$NAME'" | Out-File -FilePath $PATHPREFIX/vm_$CLUSTER.py -Append
+    "NAME = '$NAME'" | Out-File -FilePath $PATHPREFIX/virtual_maschines/vm_$CLUSTER.py -Append
     
     $VCPUS = $VM.NumCpu
-    "VCPUS = '$VCPUS'" | Out-File -FilePath $PATHPREFIX/vm_$CLUSTER.py -Append
+    "VCPUS = '$VCPUS'" | Out-File -FilePath $PATHPREFIX/virtual_maschines/vm_$CLUSTER.py -Append
 
     $MEMORY = $VM.MemoryMB
-    "MEMORY = '$MEMORY'" | Out-File -FilePath $PATHPREFIX/vm_$CLUSTER.py -Append
+    "MEMORY = '$MEMORY'" | Out-File -FilePath $PATHPREFIX/virtual_maschines/vm_$CLUSTER.py -Append
 
     $DISK = [math]::round((Get-HardDisk -vm $VM | Measure-Object -Sum CapacityGB).Sum)
-    "DISK = '$DISK'" | Out-File -FilePath $PATHPREFIX/vm_$CLUSTER.py -Append
+    "DISK = '$DISK'" | Out-File -FilePath $PATHPREFIX/virtual_maschines/vm_$CLUSTER.py -Append
 
     $STATUS = $VM.PowerState
-    "STATUS = '$STATUS'" | Out-File -FilePath $PATHPREFIX/vm_$CLUSTER.py -Append
+    "STATUS = '$STATUS'" | Out-File -FilePath $PATHPREFIX/virtual_maschines/vm_$CLUSTER.py -Append
  
     $ROLE = ((Get-TagAssignment -Entity $VM -Category 'Role')).Tag.Name
-    IF ($ROLE) { "ROLE = '$ROLE'" | Out-File -FilePath $PATHPREFIX/vm_$CLUSTER.py -Append }
+    IF ($ROLE) { "ROLE = '$ROLE'" | Out-File -FilePath $PATHPREFIX/virtual_maschines/vm_$CLUSTER.py -Append }
 
-    "" | Out-File -FilePath $PATHPREFIX/vm_$CLUSTER.py -Append
+    "" | Out-File -FilePath $PATHPREFIX/virtual_maschines/vm_$CLUSTER.py -Append
   }
 
-"" | Out-File -FilePath $PATHPREFIX/vm_$CLUSTER.py -Append
+"" | Out-File -FilePath $PATHPREFIX/virtual_maschines/vm_$CLUSTER.py -Append
 }
 
