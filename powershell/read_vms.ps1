3009daede36292f90d400ca002ce7f73ca066f37
e-atli <51813781+e-atli@users.noreply.github.com>
2019-11-14 11:16:02 +0100

    Update read_vms.ps1

--- powershell/read_vms.ps1
+++ powershell/read_vms.ps1
@@ -1,9 +1,9 @@
 #!/usr/bin/pwsh
 
-$VCENTER = $($args[0])
-$USER = $($args[1])
-$PASS = $($args[2])
-$PATHPREFIX = $($args[3])
+$VCENTER = $args[0]
+$USER = $args[1]
+$PASS = $args[2]
+$PATHPREFIX = $args[3]
 
 If ($VCENTER = $Null) { Exit }
 If ($USER = $Null) { Exit }
@@ -11,13 +11,13 @@ If ($PASS = $Null) { Exit }
 If ($PATHPREFIX = $Null) { $PATHPREFIX = "." }
 
 # Verbinde zum vCenter
-Write-Host $VCENTER
-Write-Host $USER
-Write-Host $PASS
-Write-Host $PATHPREFIX
+Write-Host "$VCENTER"
+Write-Host "$USER"
+Write-Host "$PASS"
+Write-Host "$PATHPREFIX"
 
 exit 
-Connect-VIServer -Server $VCENTER -User $USER -Password $PASS | Out-Null
+Connect-VIServer -Server "$VCENTER" -User "$USER" -Password "$PASS" | Out-Null
 
 # Cluster ermitteln
 $CLUSTERS = Get-Cluster
