4b0dc3d7775e22a3c70749f1ed59e5f5bbc53d9d
e-atli <51813781+e-atli@users.noreply.github.com>
2019-11-14 09:53:22 +0100

    Update read_vms.ps1

--- powershell/read_vms.ps1
+++ powershell/read_vms.ps1
@@ -1,37 +1,37 @@
 #!/usr/bin/pwsh
 
 Connect-VIServer -Server $args[0] -User $args[1] -Password $args[2] | Out-Null
+PATHPREFIX="$args[3]"
 
 # Cluster ermitteln
 $CLUSTERS = Get-Cluster
 
 Foreach ($CLUSTER in $CLUSTERS) {
-  #"CLUSTER = '$CLUSTER'" | Tee-Object -FilePath vm_$CLUSTER.py
-  "CLUSTER = '$CLUSTER'" | Out-File -FilePath vm_$CLUSTER.py
+  "CLUSTER = '$CLUSTER'" | Out-File -FilePath $PATHPREFIX/vm_$CLUSTER.py
   
   $VMS = Get-VM -Location $CLUSTER
 
   Foreach ($VM in $VMS) {
     $NAME = $VM.Name
-    "NAME = '$NAME'" | Out-File -FilePath vm_$CLUSTER.py -Append
+    "NAME = '$NAME'" | Out-File -FilePath $PATHPREFIX/vm_$CLUSTER.py -Append
     
     $VCPUS = $VM.NumCpu
-    "VCPUS = '$VCPUS'" | Tee-Object -FilePath vm_$CLUSTER.py -Append
+    "VCPUS = '$VCPUS'" | Out-File -FilePath $PATHPREFIX/vm_$CLUSTER.py -Append
 
     $MEMORY = $VM.MemoryMB
-    "MEMORY = '$MEMORY'" | Tee-Object -FilePath vm_$CLUSTER.py -Append
+    "MEMORY = '$MEMORY'" | Out-File -FilePath $PATHPREFIX/vm_$CLUSTER.py -Append
 
     $DISK = [math]::round((Get-HardDisk -vm $VM | Measure-Object -Sum CapacityGB).Sum)
-    "DISK = '$DISK'" | Tee-Object -FilePath vm_$CLUSTER.py -Append
+    "DISK = '$DISK'" | Out-File -FilePath $PATHPREFIX/vm_$CLUSTER.py -Append
 
     $STATUS = $VM.PowerState
-    "STATUS = '$STATUS'" | Tee-Object -FilePath vm_$CLUSTER.py -Append
+    "STATUS = '$STATUS'" | Out-File -FilePath $PATHPREFIX/vm_$CLUSTER.py -Append
  
     $ROLE = ((Get-TagAssignment -Entity $VM -Category 'Role')).Tag.Name
-    IF ($ROLE) { "ROLE = '$ROLE'" | Tee-Object -FilePath vm_$CLUSTER.py -Append }
+    IF ($ROLE) { "ROLE = '$ROLE'" | Out-File -FilePath $PATHPREFIX/vm_$CLUSTER.py -Append }
 
-    "" | Tee-Object -FilePath vm_$CLUSTER.py -Append
+    "" | Out-File -FilePath $PATHPREFIX/vm_$CLUSTER.py -Append
   }
 
-"" | Tee-Object -FilePath vm_$CLUSTER.py -Append
+"" | Out-File -FilePath $PATHPREFIX/vm_$CLUSTER.py -Append
 }
