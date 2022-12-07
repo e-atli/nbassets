c59dc0b0da665b7246cb968d92f1b3226a9e9f72
boris-maier <51813774+boris-maier@users.noreply.github.com>
2022-12-07 17:03:55 +0100

    Create dt_poweredge-r750.py

---
+++ device_types/dell-emc/poweredge_g14/dt_poweredge-r750.py
@@ -0,0 +1,11 @@
+MFGSLUG = "dell-emc"
+SLUG = "poweredge-r750"
+MODEL = "PowerEdge R750"
+U_HEIGHT = "2"
+IS_FULL_DEPTH = True
+
+POWER_PORT = "PS1"
+POWER_PORT = "PS2"
+
+IFACE_FF = '1000'
+OOB_MGMT = 'iDRAC'
